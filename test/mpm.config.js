695c013904deee839fe69ab1d7edad9d30380a49
yanglvfeng <ylvfeng@gmail.com>
2022-09-02 20:04:33 +0800

    add 多项目脚本运行

--- test/mpm.config.js
+++ test/mpm.config.js
@@ -1,26 +1,36 @@
-module.exports = function createConfig(){
-  return {
-    "errorLogPath": "./logs",
-    "branch": "0.0.1",
-    "type": "add",
-    "projects": [
-
-    ],
-    "beforeWork": [
-        'mpm -v'
-    ],
-    "afterWork": [
-      'mpm -v'
-    ],
-    "noneScripts": [
-      "mpm --time=5",
-      "mpm --time=4",
-      "mpm --read=./test/packInfo.json",
-      "mpm -v",
-      "mpm -h",
-      "mpm --test",
-      "mockMpm --test"
-    ]
-  }
+module.exports = function createConfig() {
+    return {
+        "createLog":false,
+        "errorLogPath": "./logs",
+        "operations": [
+            'git init',
+            "git add .",
+            "git status",
+            "git commit -m \"this is commit 1\"",
+            "git log",
+            // "git branch 0.0.1",
+            // "git branch -a"
+        ],
+        "projects": [
+            './project1',
+            './project2',
+            './project3'
+        ],
+        "beforeWork": [
+            // 'mpm -v'
+        ],
+        "afterWork": [
+            // 'mpm -v'
+        ],
+        "noneScripts": [
+            "mpm --time=5",
+            "mpm --time=4",
+            "mpm --read=./test/packInfo.json",
+            "mpm -v",
+            "mpm -h",
+            "mpm --test",
+            "mockMpm --test"
+        ]
+    }
 
 }
